44a23827e7f3ec7e93cc0d88b8d298f5dbfb3d84
Pavlo.Shevtsov <paulshevtsov@gmail.com>
2024-04-26 17:02:42 +0300

    Add state bucket

---
+++ variables.tf
@@ -0,0 +1,27 @@
+variable "GOOGLE_REGION" {
+    type = string
+    description = "GCP Region"
+}
+variable "GOOGLE_PROJECT" {
+    type = string
+    description = "GCP project"
+}
+
+variable "GITHUB_OWNER" {
+    type = string
+    description = "GitHub User"
+}
+
+variable "GITHUB_TOKEN" {
+    type = string
+    description = "GitHub access token"
+}
+
+variable "FLUX_GITHUB_REPO" {
+    type = string
+    description = "FLuxCD repo"
+}
+
+
+
+
